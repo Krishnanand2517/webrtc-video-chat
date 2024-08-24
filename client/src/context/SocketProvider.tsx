693ba83469cf99125df69fbbf6ace2d935369478
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-08-25 00:55:23 +0530

    deploy: Update socket server url in frontend

--- client/src/context/SocketProvider.tsx
+++ client/src/context/SocketProvider.tsx
@@ -4,7 +4,10 @@ import { io, Socket } from "socket.io-client";
 const SocketContext = createContext<Socket | null>(null);
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
-  const socket = useMemo(() => io("http://localhost:8000"), []);
+  const socket = useMemo(
+    () => io("http://webrtc-videochat-server.ap-south-1.elasticbeanstalk.com"),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
